827618056bc47f593c035ebbd473399351bd2b46
Tatiana Malygina <merlettaia@gmail.com>
2015-02-19 17:39:07 +0300

    added main script and modified logging

---
+++ python_scripts/main_pipeline.py
@@ -0,0 +1,10 @@
+from triangulator import *
+#import ala_scan
+
+if __name__ == "__main__":
+    pair_of_chains = read_pdb_info('../test_data/2OSL.pdb')
+    surface1 = process_chain(pair_of_chains[0])
+    surface2 = process_chain(pair_of_chains[1])
+    print("got surfaces, next should find dist between them")
+    dist = hausdorff_distance(surface1, surface2)
+    print(dist)
--- python_scripts/triangulator.py
+++ python_scripts/triangulator.py
@@ -4,9 +4,6 @@ import timeit
 import logging
 import os
 
-logging.basicConfig(filename="logs/" + os.path.splitext(os.path.basename(__file__))[0] + ".log", level=logging.DEBUG)
-
-
 
 ######################################
 # decorators (for benchmarking)
@@ -208,6 +205,7 @@ def hausdorff_distance(surface1, surface2):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="logs/" + os.path.splitext(os.path.basename(__file__))[0] + ".log", level=logging.DEBUG)
     logging.debug("============\nCalled triangulator")
     pair_of_chains = read_pdb_info('../test_data/2OSL.pdb')
     surface1 = process_chain(pair_of_chains[0])
