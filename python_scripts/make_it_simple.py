dcbebb375f46fa1f1b2bdbd4d1d0f823aa48fb1a
Tatiana Malygina <merlettaia@gmail.com>
2015-04-13 22:53:44 +0300

    random changes

--- python_scripts/make_it_simple.py
+++ python_scripts/make_it_simple.py
@@ -53,6 +53,14 @@ def print_accumulated():
 def read_pdb_info(filename, chain1= 'L', chain2 = 'H'):
     parser = PDBParser()
     structure = parser.get_structure('', filename)
+    model = structure[0]
+    dssp = DSSP(model, filename, dssp='mkdssp')
+    # DSSP data is accessed by a tuple (chain_id, res_id)
+    a_key = list(dssp)[2]
+    for (key, value, v, e, r,u) in dssp:
+        print value
+    # residue object, secondary structure, solvent accessibility,
+    # relative accessiblity, phi, psi
     return (
         list(structure[0][chain1].get_atoms()),
         list(structure[0][chain2].get_atoms()) # ,
@@ -93,10 +101,10 @@ def construct_triangle_dict(triangulation):
 # second arg - to test on very-very small subset
 # should return triangulation, with some additional objects - neighbours of edge and neighbours of vertex
 @timed
-def process_chain(points, points_no = 32):
+def process_chain(points, points_no = 0):
     logging.debug("process_chain called for " + str(points_no) + " atoms")
     from scipy.spatial import Delaunay, KDTree
-    p0 = points[: points_no]
+    p0 = points[: (points_no if points_no > 0 else len(points))]
     p = np.array(map(lambda x : (x.get_vector()._ar), p0))
     #p = np.array(points[:points_no])
     #print(p[0].get_coord()[1])
@@ -153,23 +161,35 @@ class DTNode:
         self.points_set = set()
         for p in points:
             self.points_set.add(p)
+        assert(len(points)==3)
+        assert(len(self.points_set)==3)
         #print(self.points)
     def intersect(self, other):
         return np.intersect1d(self.points, other.points)
     def __eq__(self, other):
-        return isinstance(other, DTNode) and len(np.setdiff1d(self.points, other.points)) == 0
+        assert(len(self.points) == len(other.points) ==3)
+        #try:
+        cond3 = len(np.setdiff1d(self.points, other.points)) == 0
+        return isinstance(other, DTNode) and isinstance(self, DTNode) and cond3
+        #except Exception:
+        #    print(self.points)
+        #    print(other.points)
+        #    return True
     def __ne__(self, other):
         return not self.__eq__(other)
     def __hash__(self):
         return hash(tuple(self.points_set))
 def get_raduis(atom_name):
-    atom_radii = {"C": 1.7, "N": 1.55, "H": 1.2, "O": 1.52}#without hydrogen, simple van der Waals radii
+    atom_radii = {"C": 1.7, "N": 1.55, "H": 1.2, "O": 1.52, "S": 1.8}#without hydrogen, simple van der Waals radii
     if (atom_name in atom_radii):
         return atom_radii[atom_name]
+    print atom_name
+    #return 1.5
     #TODO: raise exception if atom is unknown
     #TODO: process situations where hydrogen doesn't appear in pdb file
 class DTGraph:
     def __init__(self, surface):
+        self.path_result = False
         self.nodes_map = {}
         self.visited = {}
         #print(surface)
@@ -180,53 +200,75 @@ class DTGraph:
         #print(surface[0])
         #print(idx)
         for k in surface[1].simplices:
+            assert(len(k)==4)
             tetrahedra_nodes = np.asarray([DTNode(m) for m in k[idx]])
             for [p0, p1] in tetrahedra_nodes[idx2]:
+                assert(p0 != p1)
                 t_edge = np.vectorize(lambda x: surface[0][x])(p0.intersect(p1))
-                if not p0 in self.nodes_map:
-                    self.nodes_map[p0] = set()
+                assert(len(t_edge) ==2)
                 if not p0 in self.visited:
                     self.visited[p0] = False
-                if not p1 in self.nodes_map:
-                    self.nodes_map[p1] = set()
                 if not p1 in self.visited:
                     self.visited[p1] = False
                 if check_edge(*t_edge):
+                    if not p0 in self.nodes_map:
+                        self.nodes_map[p0] = set()
+                    if not p1 in self.nodes_map:
+                        self.nodes_map[p1] = set()
                     self.nodes_map[p0].add(p1)
                     self.nodes_map[p1].add(p0)
-
                 #print t_edge
         #print(self.nodes_map)
+        #print([k for k in self.visited if not isinstance(k, DTNode)])
         #print(surface[1].simplices[:, idx])
         #f = np.vectorize(lambda x: set(x.flat))
         #g = np.vectorize(lambda x: x)
         #for k in surface[1].simplices[:, idx]:
         #    print k
-    #recursive method for finding all paths in graph
-    def get_path(self, start_node):
+    # method adds all child nodes and returns current node (from queue)
+    def get_path_fragment(self, queue):
+        if len(queue) == 0:
+            return []
+        start_node = queue.pop()
         self.visited[start_node] = True
-        result = []
+        if not start_node in self.nodes_map:
+            return []
         for next_node in self.nodes_map[start_node]:
-            if not self.visited[next_node]:
-                result.append(next_node)
-                result.extend(self.get_path(next_node))
-        return result
+            #print(next_node)
+            if not next_node in self.visited:
+                if not self.visited[next_node]:
+                    queue.append(next_node)
+        return [start_node]
     def find_pockets(self, triangles):
-        nodes = set()
-        # print(triangles)
-        return (np.unique(np.asarray([ x.points for triangle in triangles for x in self.get_path(DTNode(triangle)) ]))[0])
+        self.queue = [DTNode(triangle) for triangle in triangles]
+        data = []
+        while (len(self.queue) > 0):
+            data.extend(self.get_path_fragment(self.queue))
+        result = np.asarray([x.points for x in data])
+        if(len(result) == 0):
+            return result
+        return np.unique(result)[0]
+        #return #(np.unique(
+        #return np.asarray([ x.points for triangle in triangles for x in self.get_path(DTNode(triangle)) ])
+        #)[0])
 
 
 if __name__ == "__main__":
     logging.basicConfig(filename="logs/" + os.path.splitext(os.path.basename(__file__))[0] + ".log", level=logging.DEBUG)
     logging.debug("============\nCalled simple script")
-    pair_of_chains = read_pdb_info('../test_data/2OSL.pdb')
+    pair_of_chains = read_pdb_info(os.path.abspath('../test_data/2OSL.pdb'))
     surface1 = process_chain(pair_of_chains[0])
     surface2 = process_chain(pair_of_chains[1])
-    cutoff = 32
+    cutoff = 5.0
+    #while cutoff < 42.0:
+    #    print(cutoff)
+    #    cutoff += 1
     triangles = find_interface_triangles(surface1, surface2, cutoff)
     G = DTGraph(surface1)
+    #nodes = G.find_pockets(triangles)
+    #print(nodes)
     nodes = np.setdiff1d(G.find_pockets(triangles), triangles)
+    #    if (len(nodes) > 0):
     print(to_aa(triangles, surface1))#this returns interface aminoacids with atoms located near surface2
     print(to_aa(nodes, surface1))#this retuns all aminoacids forming pockets except ones shown previously
     print("got surfaces, next should find dist between them")
