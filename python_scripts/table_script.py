784abb0b088e40fd272c457664163e148cd4ca00
Tatiana Malygina <merlettaia@gmail.com>
2015-05-26 19:28:34 +0300

    loaded structures for demo database once again

--- python_scripts/table_script.py
+++ python_scripts/table_script.py
@@ -24,5 +24,27 @@ TableLine = namedtuple('TableLine', [
                 'ehraLength', # number of aminoacids in hotspot regions in mutated chain
                 'maskedLength', # number of masked aminoacids
                 'foundHotspots',
-                'totalHotspots'
+                'totalHotspots',
+                'method' # method used to collect this data: string, either EHRA or other methods
                 ])
+
+
+import csv, optparse
+from rcsb_loader import fetch_pdb
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser()
+    parser.add_option('--csv', dest = 'csv', default = "",
+        help = 'the csv file containing mutations information')
+    parser.add_option('--pdb_folder', dest="pdb_folder", default="temp_pdb",
+        help = "folder for saving pdb files from rcsb server")
+    (options,args) = parser.parse_args()
+    with open(options.csv) as infile:
+        reader = csv.DictReader(infile)
+        reader.fieldnames[:] = map(lambda x: x.translate(None, '#:(),'), reader.fieldnames)
+        print(reader.fieldnames)
+        Data = namedtuple('Data', reader.fieldnames)
+        tuples = [Data(**row) for row in reader]
+    names = set(map(lambda data:data.PDB_ID, tuples))
+    for pdb in names:
+        fetch_pdb(pdb, options.pdb_folder)
